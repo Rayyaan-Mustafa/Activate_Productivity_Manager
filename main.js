d07b1ca4b483f720faff7c901bb4196b974ed097
Anthony Gao <gamer@LAPTOP-G5OO6GEC.localdomain>
2020-06-01 00:26:13 -0700

    added very glitchy calendar

--- main.js
+++ main.js
@@ -12,35 +12,53 @@ function openActivateTab(evt, ActivateTabName) {
   }
   document.getElementById(ActivateTabName).style.display = "block";
   evt.currentTarget.className += " active";
+  if (ActivateTabName == 'Calendar') {
+    //loadCalendarYears();
+  }
 }
 
 
 //calendar stuff
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var startYear = 2010;
+var endYear = 2030;
+var month = 0;
+var year = 0;
 
-function drawCalendarMonths() {
+function loadCalendarMonths() {
   for (var i = 0; i < months.length; i++) {
     var doc = document.createElement("div");
+    doc.setAttribute('id', months[i]);
     doc.innerHTML = months[i];
-    doc.classList.add("dropdown-item");
+    //doc.classList.add("dropdown-item");
 
     doc.onclick = (function () {
       var selectedMonth = i;
+
       return function () {
         month = selectedMonth;
-        document.getElementById("curMonth").innerHTML = months[month];
         loadCalendarDays();
+        hideCalendarMonths();
         return month;
       }
+
     })();
 
     document.getElementById("months").appendChild(doc);
   }
+
 }
-function loadYears() {
-  // whichever date range makes the most sense
-  var startYear = 1900;
-  var endYear = 2022;
+
+function hideCalendarMonths() {
+  for (var j = 0; j < months.length; j++) {
+    var elem = document.getElementById(months[j]);
+    elem.style.display = "none";
+    elem.parentNode.removeChild(elem);
+  }
+}
+
+function loadCalendarYears() {
+  document.getElementById("years").innerHTML = "";
 
   for (var i = startYear; i <= endYear; i++) {
     var doc = document.createElement("div");
@@ -60,30 +78,30 @@ function loadYears() {
     document.getElementById("years").appendChild(doc);
   }
 }
-function daysInMonth(month, year) {
-  let d = new Date(year, month + 1, 0);
-  return d.getDate();
-}
+
+
 
 function loadCalendarDays() {
   document.getElementById("calendarDays").innerHTML = "";
 
   var tmpDate = new Date(year, month, 0);
   var num = daysInMonth(month, year);
-  var dayofweek = tmpDate.getDay();
+  var dayofweek = tmpDate.getDay();       // find where to start calendar day of week
+
   for (var i = 0; i <= dayofweek; i++) {
     var d = document.createElement("div");
     d.classList.add("day");
     d.classList.add("blank");
     document.getElementById("calendarDays").appendChild(d);
   }
-  // render the rest of the days
+
   for (var i = 0; i < num; i++) {
     var tmp = i + 1;
     var d = document.createElement("div");
     d.id = "calendarday_" + i;
     d.className = "day";
     d.innerHTML = tmp;
+
     document.getElementById("calendarDays").appendChild(d);
   }
 
@@ -92,6 +110,42 @@ function loadCalendarDays() {
   document.getElementById("calendarDays").appendChild(clear);
 }
 
+function daysInMonth(month, year) {
+  var d = new Date(year, month + 1, 0);
+  return d.getDate();
+}
+
+window.addEventListener('load', function () {
+  var date = new Date();
+  month = date.getMonth();
+  year = date.getFullYear();
+  document.getElementById("curMonth").innerHTML = months[month];
+  document.getElementById("curYear").innerHTML = year;
+  //loadCalendarYears();
+  loadCalendarDays();
+});
+
+// //this function will find today's date
+// function calendar() {
+//   var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//   var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//   var d = new Date();
+//   setText('calendar-day', day[d.getDay()]);
+//   setText('calendar-date', d.getDate());
+//   setText('calendar-month-year', month[d.getMonth()] + ' ' + (1900 + d.getYear()));
+// };
+
+// //this function will set the text value of 
+// tags
+// function setText(id, val) {
+//   if (val < 10) {
+//     val = '0' + val;    //add leading 0 if val < 10
+//   }
+//   document.getElementById(id).innerHTML = val;
+// };
+
+// //call calendar() when page load
+// window.onload = calendar;
 
 // function createDropdown(identification, count) { 
 //     for(var i=1; i<=count; i++){
