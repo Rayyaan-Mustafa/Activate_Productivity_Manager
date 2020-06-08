eb1f03b8460e5d48a2ed939ed8b0fdc443c60187
Rayyaan-Mustafa <Rayyaan-Mustafa@users.noreply.github.com>
2020-06-08 16:07:57 -0700

    decorator pattern implemented

--- main.js
+++ main.js
@@ -433,12 +433,18 @@ function displayPolarChart() {//MOST LIKE BETTER TO IMPLEMENT THIS AS A BAR CHAR
           total_hrs_by_event("sleep"),
           total_hrs_by_event("social-time"),
           total_hrs_by_event("work")
-        ]
+        ],
+        backgroundColor: bgTheme,
+        borderColor: bcTheme
       }],
     },
     options: {
       responsive: true,
-      maintainAspectRatio: true
+      maintainAspectRatio: true,
+      title: {
+        display:true,
+        text: 'Total Hours By Event'
+      }
     }
   })
 
@@ -453,7 +459,7 @@ function displayBarChart() {
     data: {
       labels: ["downtime", "eating", "exercise", "family-time", "homework", "productive-work", "reading", "sleep", "social-time", "work"],
       datasets: [{
-        data: [
+                data: [
           total_hrs_by_event("downtime"),
           total_hrs_by_event("eating"),
           total_hrs_by_event("exercise"),
@@ -464,17 +470,24 @@ function displayBarChart() {
           total_hrs_by_event("sleep"),
           total_hrs_by_event("social-time"),
           total_hrs_by_event("work")
-        ]
+        ],
+        backgroundColor: bgTheme,
+        borderColor: bcTheme
       }],
     },
     options: {
       responsive: true,
-      maintainAspectRatio: true
+      maintainAspectRatio: true,
+      title: {
+        display:true,
+        text: 'Total Hours By Event'
+      }
     }
   })
 }
 
 function displayPieChart() {
+
  if(window.chart) {window.chart.destroy();} 
   let pieChart = document.getElementById('Chart').getContext('2d');
 
@@ -494,14 +507,85 @@ function displayPieChart() {
           total_hrs_by_event("sleep"),
           total_hrs_by_event("social-time"),
           total_hrs_by_event("work")
-        ]
+        ],
+        backgroundColor: bgTheme(),
+        borderColor: bcTheme()
       }],
     },
     options: {
       responsive: true,
-      maintainAspectRatio: true
+      maintainAspectRatio: true,
+      title: {
+        display:true,
+        text: 'Total Hours By Event'
+      }
     }
   })
+}
+function bgTheme() {
+//if default colors
+if(localStorage.getItem("colorTheme") === "default"){
+  return [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 198, 204, 0.2)',
+          'rgba(189, 195, 204, 0.2)',
+          'rgba(117, 196, 188, 0.2)',
+          'rgba(226, 135, 145, 0.2)'
+        ]
+}
+else {
+  return [
+          'rgba(0,104,132,0.2)',
+          'rgba(237,250,157,0.2)',
+          'rgba(176,0,81,0.2)',
+          'rgba(0,0,0,0.2)',
+          'rgba(110,0,108,0.2)',
+          'rgba(91,91,91,0.2)',
+          'rgba(57,106,177,0.2)',
+          'rgba(218,124,48,0.2)',
+          'rgba(62,150,81,0.2)',
+          'rgba(146,36,40,0.2)'
+        ]
+}
+}
+
+function bcTheme() {
+  //default colors
+if(localStorage.getItem("colorTheme") === "default"){
+  return  [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 198, 204, 1)',
+          'rgba(189, 195, 204, 1)',
+          'rgba(117, 196, 188, 1)',
+          'rgba(226, 135, 145, 1)'
+          ]
+}
+//dark colors
+else {
+  return  [
+          'rgba(0,104,132,1)',
+          'rgba(237,250,157,1)',
+          'rgba(176,0,81,1)',
+          'rgba(0,0,0,1)',
+          'rgba(110,0,108,1)',
+          'rgba(91,91,91,1)',
+          'rgba(57,106,177,1)',
+          'rgba(218,124,48,1)',
+          'rgba(62,150,81,1)',
+          'rgba(146,36,40,1)'
+          ]
+  
+}
 }
 
 class insightManager {
@@ -777,6 +861,20 @@ function saveSettings() {
   const nameInput = document.querySelector('#changeName').value;
   localStorage.setItem("name", nameInput)
   document.querySelector('#homeName').innerHTML = localStorage.getItem("name")
+  // let darkColorTheme = document.getElementById("darkColors").checked
+  // if(darkColorTheme){
+  //   localStorage.setItem("colorTheme", "dark")
+  // }
+  // else {
+  //   localStorage.setItem("colorTheme", "default")
+  // }
+  var radioInputs = document.getElementsByName("colors");
+  if(radioInputs[1].checked){//dark
+    localStorage.setItem("colorTheme", "dark")
+  }
+  else {
+    localStorage.setItem("colorTheme", "default")
+  }
 
 }
 
@@ -788,5 +886,9 @@ function percentProductive() {
 var eventComposite = new ActivateBase();
 var eventsContainer = [];//array for each object 
 createPreviousArray();
+
+if(!localStorage.getItem("colorTheme")){
+  localStorage.setItem("colorTheme",'default')
+}
 
 
